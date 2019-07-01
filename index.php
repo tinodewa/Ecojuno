894a305b2c5503fbf8245a7419318f043f60ac9f
tinodewa <dtino0812@gmail.com>
2019-07-01 13:53:28 +0700

    add ig and fb link to footer

--- index.php
+++ index.php
@@ -418,12 +418,12 @@
                 <div class="col-md-4 col-xl-3">
                     <div class="footer-icon-box">
                         <img src="assets/img/instagram-logo.png" alt="instagram logo" class="footer-icon">
-                        <a href="https://www.instagram.com/accounts/login/?hl=en"
+                        <a href="https://www.instagram.com/ecojuno/"
                             class="footer-icon-text">Ecojunonatural</a>
                     </div>
                     <div class="footer-icon-box">
                         <img src="assets/img/facebook-logo.png" alt="instagram logo" class="footer-icon">
-                        <a href="https://www.facebook.com/" class="footer-icon-text">Ecojunonatural</a>
+                        <a href="https://www.facebook.com/ecojuno/" class="footer-icon-text">Ecojunonatural</a>
                     </div>
                 </div>
             </div>
