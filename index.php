9d89c0498a86a8a7a6bc0cbfe7d4a380854ee699
T-Ibnu <ibnuspeedster@gmail.com>
2019-07-02 11:19:57 +0700

    css versioning

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
     <!-- link -->
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=2">
 
     <!-- title -->
     <title>Ecojuno</title>
